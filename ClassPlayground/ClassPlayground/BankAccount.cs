79d153156f81fd14bfc0cbbc300127802462cae7
Tomino-pus <poboril.tom@gmail.com>
2023-12-05 11:21:37 +0100

    Classes playground

---
+++ ClassPlayground/ClassPlayground/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class BankAccount
+    {
+        public double accountNumber, balance;
+        public string holderName, currency;
+        public double Deposit()
+        {
+            int deposit = 2;
+            return balance += deposit;
+        }
+
+    }
+}
---
+++ ClassPlayground/ClassPlayground/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    public class Rectangle
+    {
+        public int width, height;
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double CalculateArea()
+        {
+            return width * height;
+        }
+        public double CalculateAspectRatio()
+        {
+            if (width == height)
+            {
+                Console.WriteLine("čtverec");
+            }
+            else if (width > height)
+            {
+                Console.WriteLine("široký");
+            }
+            else
+            {
+                Console.WriteLine("vysoký");
+            }
+            return width / height;
+            
+        }
+        public bool ContainsPoint()
+        {
+            int x = 2;
+            int y = 2;
+            if (x >= 0 && x <= width)
+                if (y >= 0 && y <= height)
+                    return true;
+            else
+            return false;
+        }
+        
+    }
+}
