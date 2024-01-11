51e682045f1f6a5bd1c679c78922b506291dbbd9
Tomino-plus <poboril.tom@gmail.com>
2024-01-11 20:26:06 +0100

    ukol3

---
+++ Game2/Game2/HadaniCisla.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class HadaniCisla
+{
+    private int CorrectNumber;
+    private bool CorrectlyGuessed;
+    int attempts = 0;
+    public HadaniCisla()
+    {
+        CorrectlyGuessed = false;
+    }
+
+    public void Play()
+    {
+        Random random = new Random();
+        Console.WriteLine("Vitej v hadani cisla!");
+        Console.WriteLine($"Pokusis se tipnout nahodne vygenerovane cislo z intervalu, ktery si vyberes");
+        int Min;
+        Console.WriteLine("Zadej cele cislo - minimum");
+        while (!int.TryParse(Console.ReadLine(), out Min) || Min < 0)
+            Console.WriteLine("Zadej kladne cele cislo");
+        int Max;
+        Console.WriteLine("Zadej cele cislo - maximum");
+        while (!int.TryParse(Console.ReadLine(), out Max) || Max < Min)
+            Console.WriteLine("Zadej cele cislo vetsi nez minimum");
+        
+        CorrectNumber = random.Next(Min, Max);
+
+        while (!CorrectlyGuessed)
+        {
+            Console.WriteLine("Zadej svuj tip");
+            string input = Console.ReadLine();
+            attempts++;
+            if (int.TryParse(input, out int guess))
+            {
+                CheckGuess(guess);
+            }
+            else
+            {
+                Console.WriteLine("Zadej prosim validni cislo");
+            }
+        }
+        if (attempts == 1) 
+        {
+            Console.WriteLine($"Vyborne! {CorrectNumber} je spravne cislo! Pro uhadnuti jsi potreboval pouze 1 pokus");
+        }
+        else if (attempts == 2 || attempts == 3 || attempts == 4)
+        {
+            Console.WriteLine($"Vyborne! {CorrectNumber} je spravne cislo! Pro uhadnuti jsi potreboval pouze 2 pokusy");
+        }
+        else 
+        {
+            Console.WriteLine($"Vyborne! {CorrectNumber} je spravne cislo! Pro uhadnuti jsi potreboval {attempts} pokusu");
+        }
+        Console.ReadKey();
+    }
+
+
+    private void CheckGuess(int guess)
+    {
+        if (guess == CorrectNumber)
+        {
+            CorrectlyGuessed = true;
+        }
+        else if (guess < CorrectNumber)
+        {
+            Console.WriteLine("Spravne cislo je vyssi nez zadane, zkus to znovu");
+        }
+        else
+        {
+            Console.WriteLine("Spravne cislo je nizsi nez zadane, zkus to znovu");
+        }
+    }
+}
---
+++ Game2/Game2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    //pri vypracovani bylo vyuzito ChatGPT
+    //mel jsem mnohem vyssi ambice, ale nevedel jsem, jak napad realizovat.
+    static void Main()
+    {
+            Console.WriteLine("Vitej v menu hry\nvyber si z nabidky, co si prejes udelat\n1 pro spusteni hry - hadani cisla\n2 pro ukonceni programu");
+            string request = "";
+            request = Console.ReadLine();
+            switch (request)
+            {
+                case "1":
+                    HadaniCisla game = new HadaniCisla();
+                    game.Play();
+                    break;
+                default:
+                    Console.WriteLine("Tak nekdy priste");
+                    break;
+            }       
+    }
+}
